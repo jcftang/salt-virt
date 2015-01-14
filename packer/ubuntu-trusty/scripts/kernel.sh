ff9798abe2489349fc6efbadb689a11c2ff92de9
Jimmy Tang <jcftang@gmail.com>
2015-01-14 17:57:01 +0000

    stick with trusty kernel for now

--- packer/ubuntu-trusty/scripts/kernel.sh
+++ packer/ubuntu-trusty/scripts/kernel.sh
@@ -2,7 +2,7 @@
 
 # install the backported kernel
 apt-get update
-apt-get install -y linux-image-generic-lts-saucy linux-headers-generic-lts-saucy
+apt-get install -y linux-image-generic-lts-trusty linux-headers-generic-lts-trusty
 
 # reboot
 echo "Rebooting the machine..."
